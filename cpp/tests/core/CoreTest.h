6613ada268d6f068d00fea7b8369b7a46c1d26d0
Yixing Lao <yixing.lao@gmail.com>
2022-06-13 22:09:11 -0700

    coretest.cppc

--- cpp/tests/core/CoreTest.h
+++ cpp/tests/core/CoreTest.h
@@ -26,11 +26,8 @@
 
 #pragma once
 
-#include <algorithm>
-#include <unordered_map>
 #include <vector>
 
-#include "open3d/core/CUDAUtils.h"
 #include "open3d/core/Device.h"
 #include "open3d/core/Dtype.h"
 #include "open3d/core/SizeVector.h"
@@ -41,137 +38,30 @@ namespace tests {
 
 class PermuteDtypesWithBool : public testing::TestWithParam<core::Dtype> {
 public:
-    static std::vector<core::Dtype> TestCases() {
-        return {
-                core::Bool,  core::UInt8,   core::Int8,    core::UInt16,
-                core::Int16, core::UInt32,  core::Int32,   core::UInt64,
-                core::Int64, core::Float32, core::Float64,
-        };
-    }
+    static std::vector<core::Dtype> TestCases();
 };
 
 // Select one device for each device type.
 class PermuteDevices : public testing::TestWithParam<core::Device> {
 public:
-    static std::vector<core::Device> TestCases() {
-        std::vector<core::Device> cpu_devices =
-                core::Device::GetAvailableCPUDevices();
-        std::vector<core::Device> cuda_devices =
-                core::Device::GetAvailableCUDADevices();
-
-        std::vector<core::Device> devices;
-        if (!cpu_devices.empty()) {
-            devices.push_back(cpu_devices[0]);
-        }
-        if (!cuda_devices.empty()) {
-            devices.push_back(cuda_devices[0]);
-        }
-
-        return devices;
-    }
+    static std::vector<core::Device> TestCases();
 };
 
 class PermuteDevicesWithSYCL : public testing::TestWithParam<core::Device> {
 public:
-    static std::vector<core::Device> TestCases() {
-        std::vector<core::Device> devices = PermuteDevices::TestCases();
-
-        std::vector<core::Device> sycl_cpu_devices =
-                core::Device::GetAvailableSYCLCPUDevices();
-        std::vector<core::Device> sycl_gpu_devices =
-                core::Device::GetAvailableSYCLGPUDevices();
-
-        if (!sycl_cpu_devices.empty()) {
-            devices.push_back(sycl_cpu_devices[0]);
-        }
-        if (!sycl_gpu_devices.empty()) {
-            devices.push_back(sycl_gpu_devices[0]);
-        }
-
-        return devices;
-    }
+    static std::vector<core::Device> TestCases();
 };
 
 class PermuteDevicePairs
     : public testing::TestWithParam<std::pair<core::Device, core::Device>> {
 public:
-    static std::vector<std::pair<core::Device, core::Device>> TestCases() {
-        std::vector<core::Device> cpu_devices =
-                core::Device::GetAvailableCPUDevices();
-        std::vector<core::Device> cuda_devices =
-                core::Device::GetAvailableCUDADevices();
-
-        cpu_devices.resize(
-                std::min(static_cast<size_t>(2), cpu_devices.size()));
-        cuda_devices.resize(
-                std::min(static_cast<size_t>(2), cuda_devices.size()));
-
-        std::vector<core::Device> devices;
-        devices.insert(devices.end(), cpu_devices.begin(), cpu_devices.end());
-        devices.insert(devices.end(), cuda_devices.begin(), cuda_devices.end());
-
-        // Self-pairs and cross pairs (bidirectional).
-        std::vector<std::pair<core::Device, core::Device>> device_pairs;
-        for (size_t i = 0; i < devices.size(); i++) {
-            device_pairs.push_back({devices[i], devices[i]});
-        }
-        for (size_t i = 0; i < devices.size(); i++) {
-            for (size_t j = 0; j < devices.size(); j++) {
-                if (i != j) {
-                    device_pairs.push_back({devices[i], devices[j]});
-                }
-            }
-        }
-
-        return device_pairs;
-    }
+    static std::vector<std::pair<core::Device, core::Device>> TestCases();
 };
 
 class PermuteDevicePairsWithSYCL
     : public testing::TestWithParam<std::pair<core::Device, core::Device>> {
 public:
-    static std::vector<std::pair<core::Device, core::Device>> TestCases() {
-        std::vector<core::Device> cpu_devices =
-                core::Device::GetAvailableCPUDevices();
-        std::vector<core::Device> cuda_devices =
-                core::Device::GetAvailableCUDADevices();
-        std::vector<core::Device> sycl_cpu_devices =
-                core::Device::GetAvailableSYCLCPUDevices();
-        std::vector<core::Device> sycl_gpu_devices =
-                core::Device::GetAvailableSYCLGPUDevices();
-
-        cpu_devices.resize(
-                std::min(static_cast<size_t>(2), cpu_devices.size()));
-        cuda_devices.resize(
-                std::min(static_cast<size_t>(2), cuda_devices.size()));
-        sycl_cpu_devices.resize(
-                std::min(static_cast<size_t>(2), sycl_cpu_devices.size()));
-        sycl_gpu_devices.resize(
-                std::min(static_cast<size_t>(2), sycl_gpu_devices.size()));
-
-        std::vector<core::Device> devices;
-        devices.insert(devices.end(), cpu_devices.begin(), cpu_devices.end());
-        devices.insert(devices.end(), cuda_devices.begin(), cuda_devices.end());
-        devices.insert(devices.end(), sycl_cpu_devices.begin(),
-                       sycl_cpu_devices.end());
-        devices.insert(devices.end(), sycl_gpu_devices.begin(),
-                       sycl_gpu_devices.end());
-
-        // Self-pairs and cross pairs (bidirectional).
-        std::vector<std::pair<core::Device, core::Device>> device_pairs;
-        for (size_t i = 0; i < devices.size(); i++) {
-            device_pairs.push_back({devices[i], devices[i]});
-        }
-        for (size_t i = 0; i < devices.size(); i++) {
-            for (size_t j = 0; j < devices.size(); j++) {
-                if (i != j) {
-                    device_pairs.push_back({devices[i], devices[j]});
-                }
-            }
-        }
-
-        return device_pairs;
-    }
+    static std::vector<std::pair<core::Device, core::Device>> TestCases();
 };
 
 class PermuteSizesDefaultStrides
@@ -179,41 +69,12 @@ class PermuteSizesDefaultStrides
               std::pair<core::SizeVector, core::SizeVector>> {
 public:
     static std::vector<std::pair<core::SizeVector, core::SizeVector>>
-    TestCases() {
-        return {
-                {{}, {}},
-                {{0}, {1}},
-                {{0, 0}, {1, 1}},
-                {{0, 1}, {1, 1}},
-                {{1, 0}, {1, 1}},
-                {{1}, {1}},
-                {{1, 2}, {2, 1}},
-                {{1, 2, 3}, {6, 3, 1}},
-                {{4, 3, 2}, {6, 2, 1}},
-                {{2, 0, 3}, {3, 3, 1}},
-        };
-    }
+    TestCases();
 };
 
 class TensorSizes : public testing::TestWithParam<int64_t> {
 public:
-    static std::vector<int64_t> TestCases() {
-        std::vector<int64_t> tensor_sizes{
-                0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15,
-                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-        // clang-format off
-        std::vector<int64_t> large_sizes{
-                (1 << 6 ) - 1, (1 << 6 ), (1 << 6 ) + 1,
-                (1 << 10) - 6, (1 << 10), (1 << 10) + 6,
-                (1 << 15) - 7, (1 << 15), (1 << 15) + 7,
-                (1 << 20) - 1, (1 << 20), (1 << 20) + 1,
-                (1 << 25) - 2, (1 << 25), (1 << 25) + 2, // ~128MB for float32
-        };
-        // clang-format on
-        tensor_sizes.insert(tensor_sizes.end(), large_sizes.begin(),
-                            large_sizes.end());
-        return tensor_sizes;
-    }
+    static std::vector<int64_t> TestCases();
 };
 
 }  // namespace tests
